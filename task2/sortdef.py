c0f1863d94ff552c1e504bd0cf1942b8b18dcf7a
Ivan Nurkov <ivnu@inbox.ru>
2022-08-31 13:45:46 +0300

    Add sort

--- task2/sortdef.py
+++ task2/sortdef.py
@@ -1,31 +1,33 @@
 from pprint import pprint 
 
-class Write_doc:
-    def __init__(self, doc):
-        self.name = doc 
+res_dickt = {}
+ready_dickt = []
+def sort(doc):
+    with open('task2/' + doc, encoding= "utf-8") as f:
+        count = 0
+        for line in f:
+            count += 1
+        res_dickt[doc] = count
+        sorted_res_val = sorted(res_dickt.values())
+        sorted_res = {}
+        for v in sorted_res_val:
+            for k in res_dickt.keys():
+                if res_dickt[k] == v:
+                    sorted_res[k] = v
+    return ready_dickt.append(sorted_res)
+
+
+                                
+def write(doc):
+    with open('task2/Ready doc', 'a', encoding= "utf-8") as w:
+        for name, count in ready_dickt[-1].items():
+            w.write(f'{name} \nКоличество строк: {count} \n')
         
-    def write(self, doc):
-        with open("task2/Ready doc", "a", encoding= "utf-8") as f:
-            with open('task2/' + doc, encoding= "utf-8") as w:
-                count = 0
-                for line in w: 
-                    count += 1
-                count_str = print(f'{doc} \nКоличество строк: {count}')
-                f.write(f'{doc} \nКоличество строк: {count} \n')
-
-    # def sort(self, doc): 
-    #     with open("task2/Ready doc", encoding= "utf-8") as f:
-    #         res = []
-    #         a=  f.readlines()
-    #         for x in a:
-    #         print(a)
+        
 
 
-
-tx1 = Write_doc('text1')
-tx1.write('text1')
-tx2 = Write_doc('text2')
-tx2.write('text2')
-tx3 = Write_doc('text3')
-tx3.write('text3')
+sort('text1')
+sort('text2')
+sort('text3')
+write('Ready doc')
 
